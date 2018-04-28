a8769f3d75917a75776daf34b640b8eac924fc11
unknown <haykelmtar@gmail.com>
2018-04-28 21:19:05 +0200

    second commit

--- back/src/main/java/org/exam/GExamApplication.java
+++ back/src/main/java/org/exam/GExamApplication.java
@@ -27,24 +27,35 @@ public class GExamApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(GExamApplication.class, args);
 		
-//		UserRestService us =ctx.getBean(UserRestService.class);
-//		RoleRepository ro =ctx.getBean(RoleRepository.class);
-//		
-//		User u1 = new User();
-//		u1.setUsername("admin");
-//		u1.setPassword("1234");
-//		User u2 = new User();
-//		u2.setUsername("user");
-//		u2.setPassword("1234");
-//		
-//		Role r1 = new Role("ADMIN", "ADMIN");
-//		Role r2 = new Role("USER", "ADMIN");
-//		
-//		us.saveUser(u1);
-//		us.saveUser(u2);
-//		ro.save(r1);
-//		ro.save(r2);
+		UserRestService us =ctx.getBean(UserRestService.class);
+		RoleRepository ro =ctx.getBean(RoleRepository.class);
+		
+		Role r1 = new Role("ADMIN", "ADMIN");
+		Role r2 = new Role("USER", "USER");
+		
+		ArrayList<Role> roleAdmin = new ArrayList<>();
+		roleAdmin.add(r1);
+		roleAdmin.add(r2);
+		
+		ArrayList<Role> roleUser = new ArrayList<>();
+		roleUser.add(r2);
+		
+		
+		
+		User u1 = new User();
+		u1.setUsername("admin");
+		u1.setPassword("1234");
+		u1.setRoles(roleAdmin);
+		User u2 = new User();
+		u2.setUsername("user");
+		u2.setPassword("1234");
+		u2.setRoles(roleUser);
 
+		ro.save(r1);
+		ro.save(r2);
+		
+		us.saveUser(u1);
+		us.saveUser(u2);
 
 		
 	}
